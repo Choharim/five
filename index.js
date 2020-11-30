9c2b1b699aecb75fc4b904f8e8af849ef4f36972
Choharim <jhl8586@naver.com>
2020-11-30 12:05:23 +0900

    Add moveToFinished,showFinishedList,makeFinishedArray,saveFinishedFn

--- index.js
+++ index.js
@@ -4,7 +4,54 @@ pendingList = document.querySelector(".pending-list"),
 finishedList = document.querySelector(".finished-list");
 
 const PENDING_LS = "pending";
+const FINISHED_LS = "finished";
 const pendingArray = [];
+const finishedArray = [];
+
+function saveFinished(){
+  localStorage.setItem(FINISHED_LS,JSON.stringify(finishedArray));
+}
+
+function makeFinishedArray(finishedTodo){
+  const finishedObj = {
+    text:finishedTodo
+  };
+  finishedArray.push(finishedObj);
+  saveFinished();
+}
+
+function showFinishedList(finishedTodo){
+  const listDiv = document.createElement("div");
+  const listLi = document.createElement("li");
+  const listDelBtn = document.createElement("button");
+  const listBackBtn = document.createElement("button");
+
+  finishedList.appendChild(listDiv);
+  listDiv.appendChild(listLi);
+  listDiv.appendChild(listDelBtn);
+  listDiv.appendChild(listBackBtn);
+  
+  listDiv.classList.add("list-container");
+
+  listDelBtn.innerHTML = `<i class="far fa-times-circle"></i>`;
+  listBackBtn.innerHTML = `<i class="fas fa-undo-alt"></i>`;
+  listLi.innerText = finishedTodo;
+
+  makeFinishedArray(finishedTodo);
+  listDelBtn.addEventListener("click",)
+}
+
+function moveToFinished(event){
+  const finishedPandingBtn = event.target;
+  const finishedPandingDiv = finishedPandingBtn.parentNode;
+  const finishedText = finishedPandingDiv.children[0].innerText;
+  const delIndex = pendingArray.findIndex(pendingTodo => pendingTodo.text === finishedText);
+  
+  showFinishedList(finishedText);
+  pendingArray.splice(delIndex,1);
+  savePending();
+  finishedPandingDiv.remove();
+}
 
 function delPending(event){
   const delPendingBtn = event.target;
@@ -47,6 +94,7 @@ function showPendingList(pendingTodo){
   listLi.innerText = pendingTodo;
 
   listDelBtn.addEventListener("click",delPending);
+  listFinishedBtn.addEventListener("click",moveToFinished);
   
   makePendingArray(pendingTodo);
 }
@@ -62,9 +110,13 @@ function submitHandle(event){
 function init(){
   const string_loadedPending = localStorage.getItem(PENDING_LS);
   const parsed_loadedPending = JSON.parse(string_loadedPending);
-
+  const string_loadedFinished = localStorage.getItem(FINISHED_LS);
+  const parsed_loadedFinished = JSON.parse(string_loadedFinished);
   if(parsed_loadedPending !== null){
     parsed_loadedPending.forEach(todo => showPendingList(todo.text));
+  }
+  if(parsed_loadedFinished !== null){
+    parsed_loadedFinished.forEach(todo => showFinishedList(todo.text));
   }
   todoForm.addEventListener("submit",submitHandle);
 }
