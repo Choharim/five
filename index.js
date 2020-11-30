53969dbe6a5ee5bd5576028e1e49f4807b328dfd
Choharim <jhl8586@naver.com>
2020-11-30 10:11:30 +0900

    Add makePendingArrayFn

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ todoInput = todoForm.querySelector(".todo-input"),
 pendingList = document.querySelector(".pending-list"),
 finishedList = document.querySelector(".finished-list");
 
+const pendingArray = [];
 
-function makePendingArray(){}
+function makePendingArray(pendingTodo){
+  const pendingObj = {
+    text:pendingTodo
+  };
+  pendingArray.push(pendingObj);
+}
 
-function showPendingList(){
+function showPendingList(todo){
   const listDiv = document.createElement("div");
   const listLi = document.createElement("li");
   const listDelBtn = document.createElement("button");
@@ -21,15 +27,17 @@ function showPendingList(){
 
   listDelBtn.innerHTML = `<i class="far fa-times-circle"></i>`;
   listFinishedBtn.innerHTML = `<i class="far fa-check-circle"></i>`;
+  listLi.innerText = todo;
   
-  makePendingArray();
+  makePendingArray(todo);
 }
 
 function submitHandle(event){
   event.preventDefault();
   if(todoInput.value !== ""){
-    showPendingList();
+    showPendingList(todoInput.value);
   }
+  todoInput.value = "";
 }
 
 function init(){
