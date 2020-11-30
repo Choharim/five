9bfeb4708ff88427df4ec84e28bf650174fb2f7b
Choharim <jhl8586@naver.com>
2020-11-30 10:04:47 +0900

    Add showPendingListFn

--- index.js
+++ index.js
@@ -3,13 +3,32 @@ todoInput = todoForm.querySelector(".todo-input"),
 pendingList = document.querySelector(".pending-list"),
 finishedList = document.querySelector(".finished-list");
 
-function showList(){
+
+function makePendingArray(){}
+
+function showPendingList(){
+  const listDiv = document.createElement("div");
+  const listLi = document.createElement("li");
+  const listDelBtn = document.createElement("button");
+  const listFinishedBtn = document.createElement("button");
+
+  pendingList.appendChild(listDiv);
+  listDiv.appendChild(listLi);
+  listDiv.appendChild(listDelBtn);
+  listDiv.appendChild(listFinishedBtn);
+  
+  listDiv.classList.add("list-container");
+
+  listDelBtn.innerHTML = `<i class="far fa-times-circle"></i>`;
+  listFinishedBtn.innerHTML = `<i class="far fa-check-circle"></i>`;
+  
+  makePendingArray();
 }
 
 function submitHandle(event){
   event.preventDefault();
   if(todoInput.value !== ""){
-    showList();
+    showPendingList();
   }
 }
 
